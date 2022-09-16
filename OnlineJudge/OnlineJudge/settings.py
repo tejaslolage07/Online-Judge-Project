679912003e08912d312a11d971aa1b6eee2362d5
Tejas Lolage <tejaslolage@gmail.com>
2022-09-16 23:35:16 +0530

    Changed some settings to include the directories of css, js and img files. Removed some clutter.

--- OnlineJudge/OnlineJudge/settings.py
+++ OnlineJudge/OnlineJudge/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'OJ/templates/OJ/js'), os.path.join(BASE_DIR, 'OJ/templates/OJ/css'), os.path.join(BASE_DIR, 'OJ/templates/OJ/img')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'OJ/templates/OJ/js'), os.path.join(BASE_DIR, 'OJ/templates/OJ/css'), os.path.join(BASE_DIR, 'OJ/templates/OJ/img'), os.path.join(BASE_DIR, 'OJ/templates/OJ/ace')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
