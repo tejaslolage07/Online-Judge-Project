b2875342b67ded06cff58eee35de5e7b4463db3c
Tejas Lolage <tejaslolage@gmail.com>
2022-09-21 16:51:38 +0530

    Re-made the changes to the codeSubmission template and wrote the code for the changes which were lost. Modified and beautified the code Submission page.

--- OnlineJudge/OJ/templates/OJ/js/codeSubmission.js
+++ OnlineJudge/OJ/templates/OJ/js/codeSubmission.js
@@ -1,13 +1,17 @@
-document.getElementById('id_userCode').addEventListener('keydown', function (e) {
-    if (e.key == 'Tab') {
-        e.preventDefault();
-        var start = this.selectionStart;
-        var end = this.selectionEnd;
-        // set textarea value to: text before caret + tab + text after caret
-        this.value = this.value.substring(0, start) +
-            "\t" + this.value.substring(end);
-        // put caret at right position again
-        this.selectionStart =
-            this.selectionEnd = start + 1;
-    }
-});
+// document.getElementById('id_userCode').addEventListener('keydown', function (e) {
+//     if (e.key == 'Tab') {
+//         e.preventDefault();
+//         var start = this.selectionStart;
+//         var end = this.selectionEnd;
+//         // set textarea value to: text before caret + tab + text after caret
+//         this.value = this.value.substring(0, start) +
+//             "\t" + this.value.substring(end);
+//         // put caret at right position again
+//         this.selectionStart =
+//             this.selectionEnd = start + 1;
+//     }
+// });
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
